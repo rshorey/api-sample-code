e7901d07006ce43920cabd0acb7886a975e0723b
Rachel Shorey <rshorey@gmail.com>
2015-02-18 18:53:22 -0500

    added error handling to movies_with_your_name

--- movies_with_your_name.py
+++ movies_with_your_name.py
@@ -1,6 +1,7 @@
 import requests #get requests
 
-firstname = "rachel" #put your first name here
+#get the user's first name
+firstname = raw_input("What is your first name? ")
 
 #hit the API.
 #use the parameter "s" to search for movies containing your fist name
@@ -11,7 +12,17 @@ r = requests.get("http://www.omdbapi.com/?s={0}&type=movie".format(firstname))
 #the .json method turns the api response into a python dictionary
 movies = r.json()
 
-#the first level of the dictionary is titled "Search".
-#loop through all the movies in the search and print our their titles
-for m in movies["Search"]:
-    print "\"{0}\" came out in {1}".format(m["Title"],m["Year"])
+if "Error" in movies: #check if there was an error
+    #check if the error is that the search was empty:
+    if movies["Error"] == "Movie not found!":
+        print "Sorry, there are no movie titles containing {0}".format(firstname)
+    else:
+        #then there was another error and we don't know what
+        print "There was an unexpected error: {0}. Try again.".format(movies["Error"])
+else:
+    print "The following movies contain your first name:"
+    #the first level of the dictionary is titled "Search".
+    #loop through all the movies in the search and print our their titles
+    for m in movies["Search"]:
+        print "\"{0}\" from {1}".format(m["Title"],m["Year"])
+
